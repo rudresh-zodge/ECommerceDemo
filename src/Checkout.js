a06b2210f4ef1a32409e0ee3c12cec6f357d639c
Rudresh <rudreshzodge@gmail.com>
2022-02-16 00:09:44 +0530

    User's Name display after login in.

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -15,6 +15,7 @@ function Checkout() {
           alt="amazonAdd"
         />
         <div>
+          <h3>Hello, {user?.email}</h3>
           <h2 className="checkout__title">Your Shopping Basket</h2>
           {basket.map((item) => (
             <CheckoutProduct
@@ -35,6 +36,4 @@ function Checkout() {
   );
 }
 
-const x = "remove this line";
-export {x}
 export default Checkout;
