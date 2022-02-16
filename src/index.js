0a85563d8ca8717bb1fc1299c627ed6588533cb8
Rudresh <rudreshzodge@gmail.com>
2022-02-17 00:54:49 +0530

    Added the Payments page.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import { StateProvider } from "./StateProvider";
 import reducer, { initialState } from "./reducer";
 
+//abc
 ReactDOM.render(
   <React.StrictMode>
     <StateProvider initialState={initialState} reducer={reducer}>
